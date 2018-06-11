e756ce3ff873a1b31f30bb7dd46293c78c3f551d
Stevan Kosijer <s.kosijer@levi9.com>
2018-06-12 00:32:19 +0200

    Adding comment feature.

--- src/main/java/foodbook/android/repository/UserRepository.java
+++ src/main/java/foodbook/android/repository/UserRepository.java
@@ -11,4 +11,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
 	User findByUsernameAndPassword(String username, String password);
 
 	User findByEmail(String s);
+
+	User findByName(String user);
 }
--- src/main/java/foodbook/android/rest/ReviewController.java
+++ src/main/java/foodbook/android/rest/ReviewController.java
@@ -7,6 +7,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,5 +27,12 @@ public class ReviewController {
 		List<ReviewDTO> reviews = reviewService.getAllReviewsByRestaurantId(restaurantId);
 
 		return new ResponseEntity<>(reviews, HttpStatus.OK);
+	}
+
+	@PostMapping(value = "/comment")
+	public ResponseEntity<?> addComment(@RequestBody ReviewDTO review) {
+		ReviewDTO rdto = reviewService.addComment(review);
+
+		return new ResponseEntity<>(rdto, HttpStatus.OK);
 	}
 }
